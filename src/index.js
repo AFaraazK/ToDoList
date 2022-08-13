89f4e0455e8fc343a73738ed98429b7dcfc8bdb3
AFaraazK <92804285+AFaraazK@users.noreply.github.com>
2022-08-12 22:16:58 -0400

    Add and Delete Tasks from Project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 const projectList = document.querySelector(".projectList");
+const taskList = document.querySelector(".taskList");
 const newProjectInput = document.querySelector(".newProject");
+const newTaskInput = document.querySelector(".newTask");
+const deleteButton = document.querySelector(".deleteProject");
 
 let projects = [];
 let activeProject;
@@ -25,7 +28,7 @@ function displayProjects(){
         // select active project list
         if(project.id == activeProject){
             projEl.style.fontWeight = "bold";
-            projEl.style.color = "goldenrod";
+            projEl.style.backgroundColor = "var(--purple)";
         }
         projectList.appendChild(projEl);
     })
@@ -34,6 +37,25 @@ function displayProjects(){
     if(activeProject != null){
         document.querySelector(".taskHeader").innerText = (projects.find(project => project.id == activeProject)).name;
     }
+}
+
+function displayTasks(){
+    
+    //clear task list
+    while(taskList.firstChild){
+        taskList.removeChild(taskList.firstChild);
+    }
+
+    if(activeProject != null){
+    //find activeProject object
+    let activeProjectObj = projects.find(project => project.id == activeProject);
+    // loop through activeProject tasks array and create a list item
+    activeProjectObj.tasks.forEach(task => {
+        let taskEl = document.createElement("li");
+        taskEl.innerHTML = task;
+        taskList.appendChild(taskEl);
+    }) }
+
 }
 
 newProjectInput.addEventListener('keypress', e => {
@@ -46,11 +68,28 @@ newProjectInput.addEventListener('keypress', e => {
     }
 })
 
+newTaskInput.addEventListener('keypress', e => {
+    if(e.key == "Enter" && newTaskInput.value != '' && activeProject != null){
+        let activeProjectObjTask = projects.find(project => project.id == activeProject);
+        activeProjectObjTask.newTask(newTaskInput.value);
+        displayTasks();
+        newTaskInput.value = '';
+    }
+})
+
 projectList.addEventListener('click', e => {
     if(e.target.tagName.toLowerCase() == 'li'){
         activeProject = e.target.dataset.projectid;
         displayProjects();
+        displayTasks();
     }
+})
+
+deleteButton.addEventListener('click', e => {
+    projects = projects.filter(project => project.id != activeProject);
+    activeProject = null;
+    displayProjects();
+    displayTasks();
 })
 
 let ProjOne = new Project("Example Project",Date.now().toString(),[]);
@@ -59,4 +98,3 @@ displayProjects();
 
 ProjOne.newTask("Get Groceries");
 ProjOne.newTask("Wash the dishes");
-console.log(ProjOne.tasks);
